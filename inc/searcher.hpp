b9712cd030d56aed800bfd7f016bea021b6f56a7
keng <k.chaig1@gmail.com>
2023-02-20 13:06:38 +0100

    Fix finish

--- inc/searcher.hpp
+++ inc/searcher.hpp
@@ -11,27 +11,30 @@
 // Create lambda function to take indices as pass by copy/reference?
 // Check if indices empty -> Yes: Quit
 // No: Pop and continue
-namespace kyc {
-class Searcher {
-  kyc::Threadpool mThreadpool{};
-  int mWorkerThreads{};
-  kyc::vector<kyc::vector<std::string>> &mData;
-  std::condition_variable_any &mCV;
-  std::shared_mutex &mMainMutex;
-  bool &mSearchFinished;
+namespace kyc
+{
+  class Searcher
+  {
+    kyc::Threadpool mThreadpool{};
+    int mWorkerThreads{};
+    kyc::vector<kyc::vector<std::string>> &mData;
+    std::condition_variable_any &mCV;
+    std::shared_mutex &mMainMutex;
+    bool &mSearchFinished;
+    std::atomic<int> mTotalCounter{};
+    int mTotalSize{};
+    bool getSearchFinished();
 
-  bool getSearchFinished();
+  public:
+    Searcher(kyc::vector<kyc::vector<std::string>> &inputVector,
+             std::condition_variable_any &mainCV, std::shared_mutex &mainMutex,
+             bool &searchFinished);
 
-public:
-  Searcher(kyc::vector<kyc::vector<std::string>> &inputVector,
-           std::condition_variable_any &mainCV, std::shared_mutex &mainMutex,
-           bool &searchFinished);
-
-  void start(int n);
-  void searchJob(const std::string &userInput, kyc::vector<std::shared_ptr<kyc::vector<std::string>>> & output_ptr);
-  kyc::vector<std::string> search(const std::string &userInput);               
-  void stop();
-  void notifyMainThread();
-};
+    void start(int n);
+    void searchJob(const std::string &userInput, kyc::vector<std::shared_ptr<kyc::vector<std::string>>> &output_ptr);
+    kyc::vector<std::string> search(const std::string &userInput);
+    void stop();
+    void notifyMainThread();
+  };
 } // namespace kyc
 #endif
